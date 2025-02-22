09e86790168445aaf2fef0c19d08fabb3ce3ba9b
Your Name <your.email@example.com>
2025-02-22 11:09:39 +0100

    new file

--- bs1.py
+++ bs1.py
@@ -1,11 +1,14 @@
 import requests
 from bs4 import BeautifulSoup
 import csv
-
-page = requests.get("https://caroutlet.es/fr/toutes-nos-voitures/?carproducer=0&car_price_min=&car_price_max=&car_year_from=2022&car_year_to=2024&car_mileage_from=&car_mileage_to=&car_fuel_type=0&car_transmission=0")
+Y1 = input ("Please enter a year in the following format Y1 : ")
+Y2 = input ("Please enter a year in the following format Y2 :")
+page = requests.get(f"https://caroutlet.es/fr/toutes-nos-voitures/?carproducer=0&car_price_min=&car_price_max=&car_year_from={Y1}&car_year_to={Y2}&car_mileage_from=&car_mileage_to=&car_fuel_type=0&car_transmission=0")
 
 def main(page):
     src = page.content 
-    print(src)
-
+    soup = BeautifulSoup(src, "lxml")
+    cars = soup.find_all("div", {'class': 'article-car'})
+    print(cars)
+    
 main(page)
